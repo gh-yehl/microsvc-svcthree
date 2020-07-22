f6c088373f0867d7d62516c5196f87e2ed6375d9
gh-yehl <54838149+gh-yehl@users.noreply.github.com>
2020-07-22 15:13:00 +0800

    update

---
+++ src/main/java/com/trace/Service3Application.java
@@ -0,0 +1,33 @@
+package com.trace;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class Service3Application {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service3Application.class, args);
+    }
+
+    @RequestMapping("/call3")
+    public String call3(){
+        System.out.println("Service-3 called...");
+        return "Service-3 is called ...";
+    }
+}
